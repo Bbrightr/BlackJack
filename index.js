31a4dfd1960af2f38e9b1983600597df9c4ea74d
Bright Obiri Gyan <74192474+Bbrightr@users.noreply.github.com>
2025-02-21 08:10:34 +0100

    BlackJack Game commit

---
+++ index.js
@@ -0,0 +1,7 @@
+//START GAME button that starts a new game or picks a card for a new game
+//NEW CARD button that picks a random card
+//message-el paragraph or ID where message is delivered
+//cards-el ID that tells player the chosen cards
+//sum-el ID that shows player the sum of the chosen cards
+
+//Player must pick randome numbers, hence app must generate random numbers to use for the game
